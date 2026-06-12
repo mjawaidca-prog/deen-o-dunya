3298f8bb30ffba8b2211a1b0cb614f530d1231f7
mjawaidca-prog <mjawaid.ca@gmail.com>
2026-06-11 22:47:15 -0600

    Handle Android edge-to-edge insets

--- android/app/src/main/java/com/deenodunya/planner/MainActivity.java
+++ android/app/src/main/java/com/deenodunya/planner/MainActivity.java
@@ -1,5 +1,28 @@
 package com.deenodunya.planner;
 
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
+        applySystemBarInsets();
+    }
+
+    private void applySystemBarInsets() {
+        View webView = getBridge().getWebView();
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
+            Insets systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(webView);
+    }
+}
