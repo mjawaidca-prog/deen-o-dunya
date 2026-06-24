c2ffcfe722df7838c6d1e7e14fc8481d6a3cefa8
mjawaidca-prog <mjawaid.ca@gmail.com>
2026-06-23 23:47:06 -0600

    Keep screen awake only during WebView media playback

--- android/app/src/main/java/com/deenodunya/planner/MainActivity.java
+++ android/app/src/main/java/com/deenodunya/planner/MainActivity.java
@@ -3,6 +3,8 @@ package com.deenodunya.planner;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,12 +12,88 @@ import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private static final String MEDIA_KEEP_AWAKE_SCRIPT =
+        "(function(){" +
+        "if(window.__ddpMediaKeepAwakeInstalled)return;" +
+        "if(typeof HTMLMediaElement==='undefined')return;" +
+        "window.__ddpMediaKeepAwakeInstalled=true;" +
+        "var notify=function(active){" +
+        "try{if(window.DDPMediaKeepAwake)window.DDPMediaKeepAwake.setKeepAwake(!!active);}catch(e){}" +
+        "};" +
+        "var update=function(){" +
+        "var media=document.querySelectorAll('audio,video');" +
+        "var active=false;" +
+        "for(var i=0;i<media.length;i++){" +
+        "if(!media[i].paused&&!media[i].ended){active=true;break;}" +
+        "}" +
+        "notify(active);" +
+        "};" +
+        "['play','playing','pause','ended','error','abort','emptied','suspend'].forEach(function(name){" +
+        "document.addEventListener(name,function(){setTimeout(update,0);},true);" +
+        "});" +
+        "var originalPlay=HTMLMediaElement.prototype.play;" +
+        "HTMLMediaElement.prototype.play=function(){" +
+        "var result=originalPlay.apply(this,arguments);" +
+        "setTimeout(update,0);" +
+        "return result;" +
+        "};" +
+        "var originalPause=HTMLMediaElement.prototype.pause;" +
+        "HTMLMediaElement.prototype.pause=function(){" +
+        "var result=originalPause.apply(this,arguments);" +
+        "setTimeout(update,0);" +
+        "return result;" +
+        "};" +
+        "document.addEventListener('visibilitychange',update);" +
+        "window.addEventListener('beforeunload',function(){notify(false);});" +
+        "setInterval(update,5000);" +
+        "setTimeout(update,0);" +
+        "})();";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(savedInstanceState);
+        installMediaKeepAwakeBridge();
         applySystemBarInsets();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        injectMediaKeepAwakeScript();
+    }
+
+    @Override
+    protected void onPause() {
+        setScreenKeepAwake(false);
+        super.onPause();
+    }
+
+    private void installMediaKeepAwakeBridge() {
+        WebView webView = getBridge().getWebView();
+        webView.addJavascriptInterface(new MediaKeepAwakeBridge(), "DDPMediaKeepAwake");
+        injectMediaKeepAwakeScript();
+    }
+
+    private void injectMediaKeepAwakeScript() {
+        WebView webView = getBridge().getWebView();
+        webView.postDelayed(() -> webView.evaluateJavascript(MEDIA_KEEP_AWAKE_SCRIPT, null), 500);
+        webView.postDelayed(() -> webView.evaluateJavascript(MEDIA_KEEP_AWAKE_SCRIPT, null), 1500);
+    }
+
+    private void setScreenKeepAwake(boolean keepAwake) {
+        if (keepAwake) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    private class MediaKeepAwakeBridge {
+        @JavascriptInterface
+        public void setKeepAwake(boolean keepAwake) {
+            runOnUiThread(() -> setScreenKeepAwake(keepAwake));
+        }
     }
 
     private void applySystemBarInsets() {
